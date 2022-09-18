43fcf0ebc01d278aae2c587e2a1a78a91d5ab26b
Poptum Charoennaew <3c14irlmao@gmail.com>
2022-09-19 00:25:51 +0700

    Add geoapify api class

---
+++ foodHelpr-mobile-app/src/apis/geoapify.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { LatLng } from "react-native-maps";
+import {GEOAPIFY_KEY} from "@env";
+
+const geoapifyAPI = axios.create({
+  baseURL: "https://api.geoapify.com/v1/geocode/",
+  timeout: 5000,
+});
+
+export default class GeoapifyAPI {
+  constructor() {}
+
+  static ReverseGeocode(coordinate: LatLng) {
+    return geoapifyAPI.get(
+      `reverse?lat=${coordinate.latitude}&lon=${coordinate.longitude}&apiKey=${GEOAPIFY_KEY}`
+    );
+  }
+}
