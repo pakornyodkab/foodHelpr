d42979141c9c0a6cb81f18481cfd5624547bbc22
Poptum Charoennaew <3c14irlmao@gmail.com>
2022-09-21 03:01:16 +0700

    Use stacknavigator in App.tsx

--- foodHelpr-mobile-app/App.tsx
+++ foodHelpr-mobile-app/App.tsx
@@ -1,18 +1,24 @@
-import React, { useEffect, useState } from "react";
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import RandomRestaurantsScreen from "./pages/RandomRestaurants/RandomRestaurants";
-import HomeScreen from "./pages/Home/Home";
+import RandomRestaurantsScreen from "./src/pages/RandomRestaurants/RandomRestaurants";
+import HomeScreen from "./src/pages/Home/Home";
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{ headerShown: false }}
+      >
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Random Restaurants" component={RandomRestaurantsScreen} />
+        <Stack.Screen
+          name="Random Restaurants"
+          component={RandomRestaurantsScreen}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
