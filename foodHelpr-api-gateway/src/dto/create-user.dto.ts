40c1f1571fce7d64c4951dbfe39dd2b537c74ab1
Pakorn Kongrit <yodpakorn2543@gmail.com>
2022-09-21 23:42:36 +0700

    create foodHelpr-api-gateway

---
+++ foodHelpr-api-gateway/src/dto/create-user.dto.ts
@@ -0,0 +1,30 @@
+import { IsNotEmpty } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  user_id: number;
+
+  @IsNotEmpty()
+  username: string;
+
+  @IsNotEmpty()
+  password: string;
+
+  @IsNotEmpty()
+  firstname: string;
+
+  @IsNotEmpty()
+  lastname: string;
+
+  @IsNotEmpty()
+  profile_picture: string;
+
+  @IsNotEmpty()
+  age: number;
+
+  @IsNotEmpty()
+  nickname: string;
+
+  @IsNotEmpty()
+  job: string;
+}
