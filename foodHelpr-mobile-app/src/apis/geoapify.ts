17b1f8872ee097ae8e02010570ca666a02d8f544
Poptum Charoennaew <3c14irlmao@gmail.com>
2022-09-22 03:26:38 +0700

    Formatting

--- foodHelpr-mobile-app/src/apis/geoapify.ts
+++ foodHelpr-mobile-app/src/apis/geoapify.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { LatLng } from "react-native-maps";
-import {GEOAPIFY_KEY} from "@env";
+import { GEOAPIFY_KEY } from "@env";
 
 const geoapifyAPI = axios.create({
   baseURL: "https://api.geoapify.com/v1/geocode/",
@@ -11,6 +11,7 @@ export default class GeoapifyAPI {
   constructor() {}
 
   static ReverseGeocode(coordinate: LatLng) {
+    console.log(coordinate);
     return geoapifyAPI.get(
       `reverse?lat=${coordinate.latitude}&lon=${coordinate.longitude}&apiKey=${GEOAPIFY_KEY}`
     );
