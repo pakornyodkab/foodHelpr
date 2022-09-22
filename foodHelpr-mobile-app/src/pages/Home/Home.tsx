4fbbad49f221d10f59df6bf5edf280b868308a23
Peemajoke <zinger.peem@gmail.com>
2022-09-23 00:23:37 +0700

    init commit

--- foodHelpr-mobile-app/src/pages/Home/Home.tsx
+++ foodHelpr-mobile-app/src/pages/Home/Home.tsx
@@ -2,13 +2,18 @@ import * as React from 'react';
 import { View, Text, Button } from 'react-native';
 
 export default function HomeScreen({ navigation }) {
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false)
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button
+      <Text>FoodHelpr</Text>
+      {!isLoggedIn && <Button
+        title="Login With Google oAuth"
+        onPress={() => setIsLoggedIn(true)} />}
+      {isLoggedIn && <Button
         title="Random Restaurants"
         onPress={() => navigation.navigate('Random Restaurants')}
-      />
+      />}
     </View>
   );
 }
