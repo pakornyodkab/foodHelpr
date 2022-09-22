a92253d0b259a1ffd92f0b69b14f425a88c0b189
Poptum Charoennaew <3c14irlmao@gmail.com>
2022-09-23 01:01:42 +0700

    update Apis

--- foodHelpr-mobile-app/src/apis/geoapify.ts
+++ foodHelpr-mobile-app/src/apis/geoapify.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { LatLng } from "react-native-maps";
 import { GEOAPIFY_KEY } from "@env";
 
-const geoapifyAPI = axios.create({
+const geoapifyApi = axios.create({
   baseURL: "https://api.geoapify.com/v1/geocode/",
   timeout: 5000,
 });
@@ -11,8 +11,7 @@ export default class GeoapifyAPI {
   constructor() {}
 
   static ReverseGeocode(coordinate: LatLng) {
-    console.log(coordinate);
-    return geoapifyAPI.get(
+    return geoapifyApi.get(
       `reverse?lat=${coordinate.latitude}&lon=${coordinate.longitude}&apiKey=${GEOAPIFY_KEY}`
     );
   }
---
+++ foodHelpr-mobile-app/src/apis/googlemaps.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { LatLng } from "react-native-maps";
+import { GOOGLEMAPS_KEY } from "@env";
+
+const googleMapsApi = axios.create({
+  baseURL: "https://maps.googleapis.com/maps/api/",
+  timeout: 5000,
+});
+
+export default class GoogleMapsApi {
+  constructor() {}
+
+  static ReverseGeocode(coordinate: LatLng) {
+    return googleMapsApi.get(
+      `geocode/json?latlng=${coordinate.latitude},${coordinate.longitude}&key=${GOOGLEMAPS_KEY}&language=en&result_type=street_address`
+    );
+  }
+}
