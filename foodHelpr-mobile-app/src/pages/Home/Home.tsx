d3790a16f91e0b2d6812f2f2c591bcf23f687b80
Peemajoke <zinger.peem@gmail.com>
2022-09-23 23:18:58 +0700

    implement google login

--- foodHelpr-mobile-app/src/pages/Home/Home.tsx
+++ foodHelpr-mobile-app/src/pages/Home/Home.tsx
@@ -1,94 +1,131 @@
 import * as React from 'react';
 import { View, Text, Image, Pressable, TextInput } from 'react-native';
 import { MaterialIcons } from "@expo/vector-icons";
+import * as Google from 'expo-auth-session/providers/google'
+import * as WebBrowser from 'expo-web-browser'
+
+WebBrowser.maybeCompleteAuthSession()
 
 export default function HomeScreen({ navigation }) {
   const [isLoggedIn, setIsLoggedIn] = React.useState(false)
-  const [username, onChangeUsername] = React.useState("")
-  const [password, onChangePassword] = React.useState("")
+  // const [username, onChangeUsername] = React.useState("")
+  // const [password, onChangePassword] = React.useState("")
+  const [accessToken, setAccessToken] = React.useState("")
+  const [userInfo, setUserInfo] = React.useState<any>()
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    androidClientId: "1089522380741-kbnf41snq90sgvdiidsdhsspjj25l1lf.apps.googleusercontent.com",
+    expoClientId: "750278734395-2chutsqvl628cqjmdlv10j20gaela8to.apps.googleusercontent.com"
+  })
+
+  React.useEffect(() => {
+    if (response?.type==="success"){
+      setAccessToken(response.authentication.accessToken)
+    }
+  }, [response])
+
+  const getUserData = async () => {
+    let userInfoResponse = await fetch("https://www.googleapis.com/userinfo/v2/me", {
+      headers: { Authorization: `Bearer ${accessToken}`}
+    })
+
+    userInfoResponse.json().then(data => {
+      setUserInfo(data)
+    })
+    console.log(userInfo)
+  }
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Image
         source={require('../../../assets/topBanner.png')}
-        style={{ height: 200, width: 400, flex: isLoggedIn ? 1 : 1.2 }}
+        style={{ height: 200, width: 400, flex: accessToken ? 1 : 1.2 }}
       />
       <View style={{ flex: 3 }}>
-        {isLoggedIn &&
+        {accessToken && getUserData() &&
           <View>
             <View className='flex-row-reverse'>
               <Image
-                source={require('../../../assets/Yod_oh_yes.jpg')}
+                // source={require('../../../assets/Yod_oh_yes.jpg')}
+                source={{uri: userInfo?.picture}}
                 style={{ height: 100, width: 100, top: -70 }}
                 className="rounded-full"
               />
             </View>
             <Text className='text-right bottom-16'>Welcome back,</Text>
-            <Text className='text-right bottom-16 text-green-500 text-2xl'>Pakorn Kongrit</Text>
+            <Text className='text-right bottom-16 text-green-500 text-2xl'>{userInfo ? userInfo.name: 'Pakorn Knogkrit'}</Text>
           </View>
         }
         <Image
-          className={`${isLoggedIn ? 'bottom-4' : 'top-16'}`}
+          className={`${accessToken ? 'bottom-4' : 'top-16'}`}
           source={require('../../../assets/FoodHelprLogo.png')}
         // style={{ height: '10%', width: '90%'}}
         />
-        {!isLoggedIn &&
-          <View>
-            <TextInput 
-              style={{ paddingHorizontal: 20 }}
-              className="top-28 h-10 border-2 text-left border-green-500 rounded-full"
-              onChangeText={onChangeUsername}
-              value={username}
-              placeholder='Username'
-              textContentType='username'
-            />
-            <TextInput 
-              style={{ paddingHorizontal: 20 }}
-              className="top-32 h-10 border-2 text-left border-green-500 rounded-full"
-              onChangeText={onChangePassword}
-              value={password}
-              placeholder='Password'
-              textContentType='password'
-              secureTextEntry
-            />
-            <Pressable
-              className="top-40 flex h-10 w-28 justify-center self-center rounded-full border-[1px] border-white bg-green-500 active:scale-95 active:bg-green-700"
-              onPress={() => setIsLoggedIn(true)}
-            >
-              <Text className="text-center font-normal text-white">
-                Sign In
-              </Text>
-            </Pressable>
-            <Pressable
-              className="top-52 flex h-10 w-40 justify-center self-center rounded-full border-[1px] border-white bg-green-500 active:scale-95 active:bg-green-700"
-              onPress={() => setIsLoggedIn(true)}
-            >
-              <Text className="text-center font-normal text-white">
-                Sign In With Google
-              </Text>
-            </Pressable>
-            <View 
-              // style={{paddingTop: 270}}
-              className="top-64 flex-row justify-end text-right font-normal text-black"
-            >
-              <Text>Need an account? </Text>
-              <Pressable
-                  // className="top-1"
-                  onPress={() => setIsLoggedIn(true)}
-                >
-                  <Text className='text-green-500 underline'>Sign Up</Text>
-                </Pressable>
-              </View>
-          </View>
+        {!accessToken &&
+          // <View>
+          //   <TextInput 
+          //     style={{ paddingHorizontal: 20 }}
+          //     className="top-28 h-10 border-2 text-left border-green-500 rounded-full"
+          //     onChangeText={onChangeUsername}
+          //     value={username}
+          //     placeholder='Username'
+          //     textContentType='username'
+          //   />
+          //   <TextInput 
+          //     style={{ paddingHorizontal: 20 }}
+          //     className="top-32 h-10 border-2 text-left border-green-500 rounded-full"
+          //     onChangeText={onChangePassword}
+          //     value={password}
+          //     placeholder='Password'
+          //     textContentType='password'
+          //     secureTextEntry
+          //   />
+          //   <Pressable
+          //     className="top-40 flex h-10 w-28 justify-center self-center rounded-full border-[1px] border-white bg-green-500 active:scale-95 active:bg-green-700"
+          //     onPress={() => setIsLoggedIn(true)}
+          //   >
+          //     <Text className="text-center font-normal text-white">
+          //       Sign In
+          //     </Text>
+          //   </Pressable>
+          //   <Pressable
+          //     className="top-52 flex h-10 w-40 justify-center self-center rounded-full border-[1px] border-white bg-green-500 active:scale-95 active:bg-green-700"
+          //     onPress={() => setIsLoggedIn(true)}
+          //   >
+          //     <Text className="text-center font-normal text-white">
+          //       Sign In With Google
+          //     </Text>
+          //   </Pressable>
+          //   <View 
+          //     // style={{paddingTop: 270}}
+          //     className="top-64 flex-row justify-end text-right font-normal text-black"
+          //   >
+          //     <Text>Need an account? </Text>
+          //     <Pressable
+          //         // className="top-1"
+          //         onPress={() => setIsLoggedIn(true)}
+          //       >
+          //         <Text className='text-green-500 underline'>Sign Up</Text>
+          //       </Pressable>
+          //     </View>
+          // </View>
+          <Pressable
+            className="top-48 flex h-10 w-40 justify-center self-center rounded-full border-[1px] border-white bg-green-500 active:scale-95 active:bg-green-700"
+            onPress={() => promptAsync()} //{showInRecents: true}
+          >
+            <Text className="text-center font-normal text-white">
+              Sign In With Google
+            </Text>
+          </Pressable>
         }
-        {isLoggedIn &&
-          <View>
+        {accessToken &&
+          <View className="flex justify-center items-center gap-1">
             <Pressable
-              className="top-12 flex h-10 w-72 justify-center self-center rounded-full border-[1px] border-white bg-green-500 active:scale-95 active:bg-green-700"
+              className="flex h-10 w-72 justify-center rounded-full border-[1px] border-white bg-green-500 active:scale-95 active:bg-green-700"
               onPress={() => navigation.navigate('Random Restaurants')}
             >
-              <View className="flex-wrap justify-center self-center">
-                <Text className=" text-white">
+              <View className="relative h-full flex justify-center">
+                <Text className="left-4 absolute text-white">
                   <MaterialIcons name="restaurant" size={24} />
                 </Text>
                 <Text className="text-center font-normal text-white">
@@ -97,12 +134,12 @@ export default function HomeScreen({ navigation }) {
               </View>
             </Pressable>
             <Pressable
-              className="top-16 flex h-10 w-72 justify-center self-center rounded-full border-[1px] border-white bg-green-500 active:scale-95 active:bg-green-700 opacity-40"
+              className="flex h-10 w-72 justify-center rounded-full border-[1px] border-white bg-green-500 active:scale-95 active:bg-green-700 opacity-40"
               onPress={() => setIsLoggedIn(true)}
               disabled
             >
-              <View className="flex-wrap justify-center self-center">
-                <Text className=" text-white">
+              <View className="relative h-full flex justify-center">
+                <Text className="left-4 absolute text-white">
                   <MaterialIcons name="microwave" size={24} />
                 </Text>
                 <Text className="text-center font-normal text-white">
@@ -111,12 +148,12 @@ export default function HomeScreen({ navigation }) {
               </View>
             </Pressable>
             <Pressable
-              className="top-20 flex h-10 w-72 justify-center self-center rounded-full border-[1px] border-white bg-green-500 active:scale-95 active:bg-green-700 opacity-40"
+              className="flex h-10 w-72 justify-center rounded-full border-[1px] border-white bg-green-500 active:scale-95 active:bg-green-700 opacity-40"
               onPress={() => setIsLoggedIn(true)}
               disabled
             >
-              <View className="flex-wrap justify-center self-center">
-                <Text className=" text-white">
+              <View className="relative h-full flex justify-center">
+                <Text className="left-4 absolute text-white">
                   <MaterialIcons name="people" size={24} />
                 </Text>
                 <Text className="text-center font-normal text-white">
@@ -125,8 +162,9 @@ export default function HomeScreen({ navigation }) {
               </View>
             </Pressable>
             <Pressable
-              className="top-36 flex h-10 w-40 justify-center self-center rounded-full border-[1px] border-green-500 bg-white active:scale-95 active:bg-green-700"
-              onPress={() => setIsLoggedIn(false)}
+              className="top-36 flex h-10 w-40 justify-center self-center rounded-full border-[1px] border-green-500 bg-white active:scale-95 active:bg-gray-300"
+              // onPress={() => setIsLoggedIn(false)}
+              onPress={() => setAccessToken("")}
             >
               <Text className="text-center font-normal text-green-500">
                 Sign Out
