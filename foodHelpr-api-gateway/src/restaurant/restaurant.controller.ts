1c70173c7ddfa99f922749b686667aad7e24d268
Pakorn Kongrit <yodpakorn2543@gmail.com>
2022-09-23 22:48:19 +0700

    add auth guard in restaurant controller in api-gateway

--- foodHelpr-api-gateway/src/restaurant/restaurant.controller.ts
+++ foodHelpr-api-gateway/src/restaurant/restaurant.controller.ts
@@ -6,7 +6,9 @@ import {
   Param,
   Post,
   Patch,
+  UseGuards,
 } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt/jwt.guard';
 import { CreateRestaurantDto } from 'src/dto/create-restaurant.dto';
 import { UpdateRestaurantDto } from 'src/dto/update-restaurant.dto';
 import { RestaurantService } from './restaurant.service';
@@ -16,21 +18,25 @@ export class RestaurantController {
   constructor(private readonly restaurantService: RestaurantService) {}
 
   @Get('get-restaurants')
+  @UseGuards(JwtAuthGuard)
   getRestaurants() {
     return this.restaurantService.getRestaurants();
   }
 
   @Post('create-restaurant')
+  @UseGuards(JwtAuthGuard)
   createRestaurant(@Body() createRestaurantDto: CreateRestaurantDto) {
     return this.restaurantService.createRestaurant(createRestaurantDto);
   }
 
   @Delete('delete-restaurant-by-restaurant-id/:id')
+  @UseGuards(JwtAuthGuard)
   deleteRestaurant(@Param('id') id: string) {
     return this.restaurantService.deleteRestaurant(id);
   }
 
   @Patch('update-restaurant-id/:id')
+  @UseGuards(JwtAuthGuard)
   updateRestaurant(
     @Param('id') id: string,
     @Body() updateRestaurantDto: UpdateRestaurantDto,
