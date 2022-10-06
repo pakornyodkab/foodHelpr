9c47c24d99d8cffb9e80c8eafb844d8c2230bc8c
Poptum Charoennaew <3c14irlmao@gmail.com>
2022-10-07 03:32:27 +0700

    Edit recipe result mock data

--- foodHelpr-mobile-app/src/pages/RandomRecipes/RecipesResult.tsx
+++ foodHelpr-mobile-app/src/pages/RandomRecipes/RecipesResult.tsx
@@ -7,6 +7,49 @@ import { FontAwesome } from "@expo/vector-icons";
 import { RecipeDetailParams } from "./RecipeDetail";
 
 import topBanner from "../../../assets/topBanner.png";
+import IRecipe from "../../models/Recipe";
+
+const mockRecipe: IRecipe = {
+  recipe_id: "asjghfajskdf",
+  name: "recipeName",
+  picture_url: [
+    "https://media.discordapp.net/attachments/918571855479189594/1020655655369052201/Yod17.jpg",
+    "https://media.discordapp.net/attachments/918571855479189594/1020655174076874763/Yod15.jpg",
+  ],
+  tags: ["Thai food", "Microwave"],
+  kcal: 2000,
+  ingredients: [
+    {
+      picture_url: [
+        "https://mw-wellness.com/wp-content/uploads/2019/05/egg.jpg",
+        "https://thethaiger.com/th/wp-content/uploads/sites/9/2018/10/iStock-547420626_M.jpg",
+      ],
+      delivery_info: [
+        {
+          delivery_type: "DELIVERY_TYPE_BIG_C",
+          url: "https://www.bigc.co.th/search?q=%E0%B9%84%E0%B8%82%E0%B9%88%E0%B9%80%E0%B8%9B%E0%B9%87%E0%B8%94",
+        },
+      ],
+      ingredient_id: "633d55a2c5f1956d089afefe",
+      name: "ไข่ไก่",
+      quantity: 10,
+      unit: "Hello",
+    },
+  ],
+  kitchen_tools: ["cookingUtensilsA", "cookingUtensilsB", "cookingUtensilsC"],
+  method:
+    "1.sadfsdf\n2.sagfsdf\n3.asdfasdf\n\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf",
+  tutorial_links: [
+    {
+      platform: "YouTube",
+      url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    },
+    {
+      platform: "facebook",
+      url: "https://www.facebook.com/gssspotted",
+    },
+  ],
+};
 
 function RecipesResult({ navigation }) {
   function handleOnPressBack() {
@@ -14,32 +57,8 @@ function RecipesResult({ navigation }) {
   }
 
   function handleRecipePress() {
-    const props: RecipeDetailParams = {
-      recipeName: "recipeName",
-      imageUrls: [
-        "https://media.discordapp.net/attachments/918571855479189594/1020655655369052201/Yod17.jpg",
-        "https://media.discordapp.net/attachments/918571855479189594/1020655174076874763/Yod15.jpg",
-      ],
-      tags: ["Thai food", "Microwave"],
-      kcal: 2000,
-      ingredients: ["ingredientA", "ingredientB", "ingredientC"],
-      cookingUtensils: [
-        "cookingUtensilsA",
-        "cookingUtensilsB",
-        "cookingUtensilsC",
-      ],
-      steps:
-        "1.sadfsdf\n2.sagfsdf\n3.asdfasdf\n\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf\n\tasdl[fkasdlgjklsadf",
-      videoUrls: [
-        {
-          platform: "YouTube",
-          url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-        },
-        {
-          platform: "facebook",
-          url: "https://www.facebook.com/gssspotted",
-        },
-      ],
+    const props = {
+      recipe: mockRecipe,
     };
     navigation.navigate(RecipeRoutes.detail, props);
   }
