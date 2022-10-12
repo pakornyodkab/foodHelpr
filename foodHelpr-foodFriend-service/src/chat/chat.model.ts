4e17de7a29810f32e161a6c38f7e6b159c7399de
Poptum Charoennaew <3c14irlmao@gmail.com>
2022-10-12 23:19:20 +0700

    Add chat timestamp

--- foodHelpr-foodFriend-service/src/chat/chat.model.ts
+++ foodHelpr-foodFriend-service/src/chat/chat.model.ts
@@ -1,10 +1,13 @@
 import * as mongoose from 'mongoose';
 
-export const ChatSchema = new mongoose.Schema({
-  roomId: { type: String, required: true },
-  senderId: { type: String, required: true },
-  message: { type: String, required: true },
-});
+export const ChatSchema = new mongoose.Schema(
+  {
+    roomId: { type: String, required: true },
+    senderId: { type: String, required: true },
+    message: { type: String, required: true },
+  },
+  { timestamps: { createdAt: true } },
+);
 
 export interface Chat {
   roomId: string;
