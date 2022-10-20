1df9ac5f65aff0bff1222eba93c73197b04c7f47
Peemajoke <zinger.peem@gmail.com>
2022-10-20 23:30:42 +0700

    fix eslint nonsense

--- foodHelpr-notification-service/src/main.ts
+++ foodHelpr-notification-service/src/main.ts
@@ -3,20 +3,27 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 import { initializeApp, credential, ServiceAccount } from 'firebase-admin';
 import * as dotenv from 'dotenv';
+// import serviceAccount from '../testnoti-sw-arch-firebase-adminsdk-ji9oy-75e07990d4.json';
 
 dotenv.config();
 
 async function bootstrap() {
+  //! no longer used.
   // const app = await NestFactory.create(AppModule);
   // await app.listen(3000);
-  initializeApp({
-    credential: credential.cert({
-      privateKey: process.env.FIREBASE_PRIVATE_KEY,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      projectId: process.env.FIREBASE_PROJECT_ID,
-    } as Partial<ServiceAccount>),
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-  });
+
+  // initializeApp({
+  //   credential: credential.cert({
+  //     privateKey: process.env.FIREBASE_PRIVATE_KEY,
+  //     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  //     projectId: process.env.FIREBASE_PROJECT_ID,
+  //   } as Partial<ServiceAccount>),
+  //   // databaseURL: process.env.FIREBASE_DATABASE_URL,
+  // });
+
+  // initializeApp({
+  //   credential: credential.cert(serviceAccount),
+  // });
 
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
